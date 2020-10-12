9ac9a247396e2ed39831647152c087e9e20512bc
mani999kandan <maniom90@gmail.com>
2020-10-12 18:57:01 +0530

    sh

---
+++ src/main/NewTest.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	public  WebDriver  driver;
+
+
+	
+
+	  @Test
+	  public void f() 
+	  {
+		  System.setProperty("webdriver.chrome.driver",
+					"chromedriver.exe");		
+					driver =new ChromeDriver();
+					driver.get("https:\\www.google.com");
+		
+				WebElement search	=driver.findElement(By.xpath ("//input[@title='Search']"));
+				search.click();
+				search.sendKeys("Om sakthi");
+	  }
+}
